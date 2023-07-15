e5c4a7b46aa02ec6e0f864514fa36d7027ea004e
kaleb pulley <pul21001@byui.edu>
2023-07-14 20:09:53 -0600

    angular to server conection for notesService

--- src/app/notes/note.model.ts
+++ src/app/notes/note.model.ts
@@ -1,11 +1,5 @@
-export class NoteModel { 
-  public id: string;
-  public subject: string;
-  public noteText: string;
-
-  constructor(id: string, subject: string, noteText: string){
-    this.id = id;
-    this.subject = subject;
-    this.noteText = noteText;
-  }
+export interface Note { 
+  id: string;
+  title: string;
+  content: string;  
 }
--- src/app/notes/notes.service.ts
+++ src/app/notes/notes.service.ts
@@ -1,36 +1,63 @@
 import { Injectable } from '@angular/core';
-import { NoteModel } from './note.model';
+import { HttpClient } from '@angular/common/http';
+import { Note } from './note.model';
+import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class NotesService {
+  private notes: Note[] = [];
+  private notesUpdated = new Subject<Note[]>();
 
-  notes: NoteModel[] = [];
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
-  getNote(): NoteModel {
-    return {id: '1'
-      , subject: 'randSubject'
-      , noteText: 'random message'}
+  getNotes(){
+    this.http
+    .get<{message: string, notes: any}>(
+      "http://localhost:3000/api/notes"
+    )
+    .pipe(map((noteData) => {
+      return noteData.notes.map((note: { title: any; content: any; _id: any; }) => {
+        return {
+          title: note.title,
+          content: note.content,
+          id: note._id
+        }
+      })
+    }))
+    .subscribe(noteData => {
+      this.notes = noteData.notes;
+      this.notesUpdated.next([...this.notes])
+    });
   }
-  getNotes(): NoteModel[] {
-    return [
-      {id: '1'
-      , subject: 'randSubject'
-      , noteText: 'random message'}
-      ,{id: '2'
-      , subject: 'randSubject2'
-      , noteText: 'random message two'}
-      ,{id: '3'
-      , subject: 'randSubject3'
-      , noteText: 'random message three'}
-    ]
+
+  getNoteUpdateListener(){
+    return this.notesUpdated.asObservable();
   }
-  addNotes(note: NoteModel){
+
+
+  addNote( title: string, content: string){
+    const note: any = { id: null, title: title, content: content };
+    this.http
+    .post<{ message: string, noteId: string }>("http://localhost:3000/api/notes", note)
+    .subscribe(responseData =>{
+      const id = responseData.noteId;
+      note.id = id;
+      this.notes.push(note);
+      this.notesUpdated.next([...this.notes]);
+    });
 
   }
+
+  deleteNote(noteId: string){
+    this.http.delete("" + noteId)
+    .subscribe(() => {
+      const updatedNotes = this.notes.filter(note => note.id !== noteId);
+      this.notes = updatedNotes;
+      this.notesUpdated.next([...this.notes]);
+    });
+  }
+
   getMaxId():any {
     let maxId = 0;
     this.notes.forEach(n => {
